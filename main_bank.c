246de052aef2c3757455dbcdce5400a4ca929368
Svetlozar Dragnev <svetlozardragnev@gmail.com>
2025-12-12 22:41:50 +0200

    created main_bank.c
    File with only main function of the bank account management system (using header file and .c file with all functions)

---
+++ main_bank.c
@@ -0,0 +1,87 @@
+#include "bank.h"
+
+// --> Main function <--
+int main()
+{
+    loadUsersFromFile();
+    int startChoice;
+
+    do
+    {
+        printf("\n === BANK ACCOUNT MANAGEMENT SYSTEM ===\n");
+        printf("\n --> MAIN MENU <--\n");
+        printf("1. Login to your account\n");
+        printf("2. Register a new user\n");
+        printf("0. Exit\n");
+        printf("\n -> Your choice: ");
+        scanf("%d", &startChoice); // Scan the user input and point it to startChoice variable
+
+        switch (startChoice)
+        {
+        case 1:
+            // LogIn logic
+            if (login())
+            {
+                // --> INSIDE MENU FOR BANK OPERATIONS <--
+                int userChoice;
+
+                // Loop while user choice is different from '0' (Exit from bank account system)
+                do
+                {
+                    printf("\n--- USER MENU (%s) ---\n", users[currentUserIndex].accountNumber);
+                    printf("1. Withdrawn Money\n");
+                    printf("2. Deposit Money\n");
+                    printf("3. Transfer to Another User\n");
+                    printf("4. Show Balance\n");
+                    printf("5. Change PIN\n");
+                    printf("0. Exit from the profile (Logout)\n");
+                    printf("Your choice: ");
+                    scanf("%d", &userChoice);
+
+                    // Invoke function for chosen bank operation and break
+                    switch (userChoice)
+                    {
+                    case 1:
+                        withdrawMoney();
+                        break;
+                    case 2:
+                        depositMoney();
+                        break;
+                    case 3:
+                        transferMoney();
+                        break;
+                    case 4:
+                        showBalance();
+                        break;
+                    case 5:
+                        changePin();
+                        break;
+                    case 0:
+                        printf("Exit from the profile...\n");
+                        break;
+                    default:
+                        printf("Invalid choice!\n");
+                    }
+                } while (userChoice != 0);
+            }
+            else
+            {
+                printf("Wrong account number and/or PIN code! Try again or contact our bank's support department.\n");
+            }
+            break;
+
+        case 2:
+            registerUser();
+            break;
+
+        case 0:
+            printf("Goodbye!\n");
+            break;
+
+        default:
+            printf("Invalid choice!\n");
+        }
+    } while (startChoice != 0);
+
+    return 0;
+}
